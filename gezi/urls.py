10291741dbec186aab55f7e097bc212a6e698b5e
bozcani <ilkerbozcan@hotmail.com>
2020-01-02 19:47:43 +0100

    Add basic views

--- gezi/urls.py
+++ gezi/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'gezi'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('trip/<int:trip_id>/', views.trip, name='trip'),
+    path('person/<int:person_id>/', views.person, name='person'),
+    path('traveler/<int:traveler_id>/', views.traveler, name='traveler')
+
 ]
--- gezi/views.py
+++ gezi/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, welcome to the Gezi App. Share your trip experience!")
+
+def trip(request, trip_id):
+    return HttpResponse("You're looking at trip %s." % trip_id)    
+
+def person(request, person_id):
+    return HttpResponse("You're looking at person %s." % person_id)    
+
+def traveler(request, traveler_id):
+    return HttpResponse("You're looking at traveler %s." % traveler_id)    
