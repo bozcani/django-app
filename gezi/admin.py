75bb3ed494d1b22b31e51dd838bce371ce81e862
bozcani <ilkerbozcan@hotmail.com>
2020-01-03 19:07:22 +0100

    Add entries to db

--- gezi/admin.py
+++ gezi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Person, Trip, Traveler
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Trip)
+admin.site.register(Traveler)
--- gezi/models.py
+++ gezi/models.py
@@ -6,11 +6,20 @@ class Trip(models.Model):
     city = models.CharField(max_length=50)
     date = models.DateTimeField('date of the trip')
 
+    def __str__(self):
+        return self.trip_name
+
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)    
-    birthdate = models.IntegerField()    
+    birthdate = models.IntegerField() 
+
+    def __str__(self):
+        return self.first_name+' '+self.last_name       
 
 class Traveler(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.person)+' goes to '+str(self.trip)       
